1b878efe5c8268c538dabaea33b0986bbf8a5423
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-12-12 15:12:07 -0800

    Changed from list to set for the external urls

--- crawler.py
+++ crawler.py
@@ -59,7 +59,7 @@ class ExternalLinkScraper:
 			elif self.check_if_url_is_target(url):
 				print("External URL detected: %s" % url)
 				print("Found from parent: %s" % parent_url)
-				self.external_urls.setdefault(parent_url, []).append(url)
+				self.external_urls.setdefault(parent_url, set([])).add(url)
 
 	def post_scrape_callback(self, res):
 		result = res.result()
@@ -88,15 +88,14 @@ class ExternalLinkScraper:
 		print("Writing all external links found to a file named: external_links.txt")
 		try:
 			with open("external_links.txt", "w+") as file:
-				print("GETTING EXTERNAL LINKS")
 				for parent_link, links in self.external_urls.items():
 					header = "External links linked from: " + parent_link + "\n"
-					print(header)
 					file.write(header)
 					for link in links:
 						content = "------> " + link + "\n"
 						print(content)
 						file.write(content)
+				print("Finished writing the external_links")
 		except Exception as e:
 			print("An exception has occured: \n %s" % e)
 
@@ -107,6 +106,7 @@ class ExternalLinkScraper:
 			with open("broken_links.txt", "w+") as file:
 				for link in self.broken_links:
 					file.write(link + "\n")
+			print("Finished writing the broken_links")
 		except Exception as e:
 			print("An exception has occured: \n %s" % e)
 
@@ -141,114 +141,3 @@ if __name__ == '__main__':
 	target_links_list = sys.argv[2:]
 	crawler = ExternalLinkScraper(start_URL, target_links_list)
 	crawler.run_crawler()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# target_links = sys.argv[2:]
-# default_target = (len(target_links) == 0)
-
-# # Uncomment this after testing
-# start_URL = sys.argv[1]
-
-# robot_txt_parser = obey_robots_protocol(start_URL)
-
-# # Deque of links to crawl next
-# new_urls_to_crawl = deque([start_URL])
-
-# # Create sets to make sure the links that are being crawled are unique
-# # Set to keep track of links in the same domain
-# local_urls = set()
-
-# # Set to keep track of broken links
-# broken_links = set()
-
-# # Set to keep track of external links
-# external_links = set()
-
-# # Set to keep track of processed URLs
-# processed_urls = set()
-
-# Keep on crawling until the deque is empty
-# while len(new_urls_to_crawl):
-# 	url_to_crawl = new_urls_to_crawl.popleft()
-# 	processed_urls.add(url_to_crawl)
-# 	if robot_txt_parser.can_fetch('*', url_to_crawl):
-# 		try:
-# 			print("B4 request: %s" % url_to_crawl)
-# 			response = requests.get(url_to_crawl, timeout=(10, 30))
-# 		except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL, 
-# 				requests.exceptions.InvalidSchema) as e:
-# 			print(e)
-# 			broken_links.add(url_to_crawl)
-# 			continue
-
-# 		url_parts = urlsplit(url_to_crawl)
-# 		base_url_to_crawl = "{0.scheme}://{0.netloc}".format(url_parts)
-# 		crawled_domain = base_url_to_crawl.replace("www.", "")
-# 		url_path = url_to_crawl[:url_to_crawl.rfind('/') + 1] if '/' in url_parts.path else url_to_crawl
-
-# 		print("Proccessing %s" % url_to_crawl)
-# 		soup = BeautifulSoup(response.text, "lxml")
-# 		for link in soup.find_all("a"):    
-# 			# extract link url from the anchor   
-# 			#print("anchors found %s" % link)
-# 			anchor = link.attrs["href"] if "href" in link.attrs else ''
-# 			if anchor.startswith('/'):   
-# 				# Relative URL -> local URL     
-# 				local_link = base_url_to_crawl + anchor        
-# 				local_urls.add(local_link)   
-# 			elif check_if_url_is_target(anchor):  
-# 				print("External link detected: %s" % anchor)      
-# 				external_links.add(anchor) 
-# 			elif crawled_domain in anchor:        
-# 				local_urls.add(anchor)    
-# 			elif not anchor.startswith('http'):        
-# 				local_link = url_path + anchor        
-# 				local_urls.add(local_link)    
-
-# 			for i in local_urls:    
-# 				if not i in new_urls_to_crawl and not i in processed_urls:        
-# 					new_urls_to_crawl.append(i)
-# 		print("finished processing %s" % url_to_crawl)
-# 	else:
-# 		print("Not allowed by robots.txt %s", url_to_crawl)
-# 	# time.sleep(1)
-
-# print("External links found: ")
-# for link in external_links:
-# 	print(link)
-
-# print("Broken links found: ")
-# for link in broken_links:
-# 	print(link)
